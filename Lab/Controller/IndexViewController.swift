259af5df5065d43e41b233d2867bea94274b5973
Ana Caroline Freitas Sampaio <ana.sampaio@10-45-48-110.0200.ifce.br.iosda.org>
2018-12-18 17:53:27 -0300

    main screen layout. created new controller. randomgreetinggenerator is working on main screen.

---
+++ Lab/Controller/IndexViewController.swift
@@ -0,0 +1,30 @@
+//
+//  IndexViewController.swift
+//  Lab
+//
+//  Created by Ana Caroline Freitas Sampaio on 18/12/2018.
+//  Copyright © 2018 Roberto Evangelista da Silva Filho. All rights reserved.
+//
+
+import UIKit
+
+class IndexViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
